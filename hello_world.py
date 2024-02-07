53a4216179417b908828fa577ced4df60c6b0bfa
Emmanuel Patricio <L192909399@cdguzman.tecnm.mx>
2024-02-06 18:45:51 -0600

    docs(hello_world): Change name from camel case to snake case

---
+++ hello_world.py
@@ -0,0 +1,27 @@
+"""This module contains a simple function to greet a user based on their input."""
+
+
+def greet_user(name):
+    """
+    Greet the user with their name.
+
+    Parameters:
+    name (str): The name of the user to greet.
+
+    Returns:
+    str: A greeting string.
+    """
+    return f"Hello, {name}!"
+
+
+def main():
+    """
+    Main function to prompt the user for their name and print a greeting.
+    """
+    name = input("Enter your name: ")
+    greeting = greet_user(name)
+    print(greeting)
+
+
+if __name__ == "__main__":
+    main()
